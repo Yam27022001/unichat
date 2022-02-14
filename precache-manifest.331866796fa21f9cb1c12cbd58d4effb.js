c244ce85e8cba79a18cc822b49a05743dc97563b
abhinav-22-tech <jainabhinav0860@gmail.com>
2022-02-14 22:46:08 +0530

    Updates

---
+++ precache-manifest.331866796fa21f9cb1c12cbd58d4effb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9b35278beb74d035299a63fdebf811",
+    "url": "/unichat/index.html"
+  },
+  {
+    "revision": "17ef1bcce87008aa5a3c",
+    "url": "/unichat/static/css/main.906d9089.chunk.css"
+  },
+  {
+    "revision": "8a24ee823ac581ea93b9",
+    "url": "/unichat/static/js/2.74214002.chunk.js"
+  },
+  {
+    "revision": "c9d20a465a79038321bd68cd78375c1f",
+    "url": "/unichat/static/js/2.74214002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ef1bcce87008aa5a3c",
+    "url": "/unichat/static/js/main.3509e806.chunk.js"
+  },
+  {
+    "revision": "dbe73014955cc44a12db",
+    "url": "/unichat/static/js/runtime-main.de7dfe23.js"
+  }
+]);
